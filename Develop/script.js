3889f076d05b5e284a669a1110ab0afb9c5c2f2b
Vaggelis <evangelodamianos@gmail.com>
2021-11-06 11:58:12 -0400

    Random password generator complete

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,88 @@
-// Assignment code here
+var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var numeric = "0123456789"
+var specialChar = "~!@#$%^&*()<>/|\{}'';:?."
 
+//NEEDS MORE COMMENTS
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword()
+{
+  var password = "";
+  var pwdLength = window.prompt("How many characters would you like your password to contain?");
+  console.log(pwdLength);
+
+  while((pwdLength < 8) || (pwdLength > 128))
+  {
+    window.alert("Your password needs to be between 8 and 128 characters long.");
+    pwdLength = window.prompt("How many characters would you like your password to contain?");
+  };
+
+  var isupperCase = window.confirm("Do you want upper case letters?");
+  console.log(isupperCase);
+
+  var islowerCase = window.confirm("Do you want lower case letters?");
+  console.log(islowerCase);
+
+  var isNumeric = window.confirm("Do you want numbers?");
+  console.log(isNumeric);
+
+  var isSpecial = window.confirm("Do you want special charcters?");
+  console.log(isSpecial);
+
+  while (!isupperCase && !islowerCase && !isNumeric && !isSpecial)
+  {
+    alert("You must choose at least one type of character to continue");
+  isupperCase = window.confirm("Do you want upper case letters?");
+  console.log(isupperCase);
+
+  islowerCase = window.confirm("Do you want lower case letters?");
+  console.log(islowerCase);
+
+  isNumeric = window.confirm("Do you want numbers?");
+  console.log(isNumeric);
+
+  isSpecial = window.confirm("Do you want special charcters?");
+  console.log(isSpecial);
+  };
+
+  var newPassword = ""
+
+  if (isupperCase)
+  {
+    newPassword += upperCase;    
+    window.alert("Your password will include upper case characters");
+    
+  };
+
+  if (islowerCase)
+  {
+    newPassword += lowerCase;
+    window.alert("Your password will include lower case characters");
+  };
+
+  if (isNumeric)
+  {
+    newPassword += numeric;    
+    window.alert("Your password will include numbers");
+  };
+
+  if (isSpecial)
+  {
+    newPassword += specialChar;
+    window.alert("Your password will include special characters");
+  };
+
+  console.log(newPassword);
+
+  for (var i = 0; i < pwdLength; i ++) {
+    password += newPassword.charAt(Math.floor(Math.random() * newPassword.length))
+  }
+
+  return password
+};
 
 // Write password to the #password input
 function writePassword() {
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # password-generator
+
+## Description: 
+
+
+## GitHub Repo:
+
+
+## Deployed App:
+
+
+## screenshot: ![Capture](https://user-images.githubusercontent.com/90728690/140615664-dadbf488-7d81-47a2-a3b7-b1adc473cca1.PNG)
+
